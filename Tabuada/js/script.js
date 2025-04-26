4cb97a1f940c637f3d378b89bdbc8ba54afb2062
Wesley Anjos <wesleyanjoszero@gmail.com>
2025-04-26 13:19:00 -0300

    feat - utilizando função para calcular

--- Tabuada/js/script.js
+++ Tabuada/js/script.js
@@ -1,21 +1,27 @@
-let tab = document.querySelector('#tab tbody')
-let valueA = 5
-tab.innerHTML = ''
-
-for (let valueB = 0; valueB <= 10; valueB++) {
+function calc() {
+    let tab = document.querySelector('#tab tbody')
+    let valueA = parseInt(document.querySelector('#valueA').value)
+    tab.innerHTML = ''
+        
+    for (let valueB = 0; valueB <= 10; valueB++) {
     let r = valueA * valueB
-    
-    let tpl = `
-                <tr>
-                    <td>${valueA}</td>
-                    <td>X</td>
-                    <td>${valueB}</td>
-                    <td>=</td>
-                    <td>${r}</td>
-                </tr>
-            `
-    
-    let tr = document.createElement('tr')
-    tr.innerHTML = tpl
-    tab.append(tr)
+        
+        let tpl = `
+        <tr>
+                        <td>${valueA}</td>
+                        <td>X</td>
+                        <td>${valueB}</td>
+                        <td>=</td>
+                        <td>${r}</td>
+                        </tr>
+                        `
+                        
+            let tr = document.createElement('tr')
+            tr.innerHTML = tpl
+            tab.append(tr)
+        }
 }
+
+calc()
+
+document.querySelector('#valueA').addEventListener('change', calc)
