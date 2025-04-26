6b771f45e67e829f3b255116540d61e9b3affb39
Wesley Anjos <wesleyanjoszero@gmail.com>
2025-04-26 12:16:51 -0300

    feat - validado os dados do formulário

--- Tela-login/js/script.js
+++ Tela-login/js/script.js
@@ -8,11 +8,17 @@ document.querySelector('#form-login').addEventListener('submit', e => {
         password
     }
 
-    console.log(json)
-    
-    let stringJSON = JSON.stringify(json)
-    console.log(stringJSON)
-    
-    let jsonParse = JSON.parse(stringJSON)
-    console.log(jsonParse)
+    if (!json.email) {
+        alert('Preencha o campo e-mail !')
+        console.error('Preencha o campo e-mail !')
+    } else if (!json.password) {
+        alert('Preencha o campo senha !')
+        console.error('Preencha o campo senha !')
+    } else {
+        alert('Dados informados com sucesso !')
+        console.info('Dados informados com sucesso !')  
+    }
+
+    // let stringJSON = JSON.stringify(json)
+    // let jsonParse = JSON.parse(stringJSON)
 })
