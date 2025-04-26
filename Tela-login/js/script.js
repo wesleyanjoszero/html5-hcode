b5100122c9ad681ef8faffadb76b5ceb0a00e34b
Wesley Anjos <wesleyanjoszero@gmail.com>
2025-04-26 12:10:02 -0300

    feat - adicioando dados em json

--- Tela-login/js/script.js
+++ Tela-login/js/script.js
@@ -2,5 +2,17 @@ document.querySelector('#form-login').addEventListener('submit', e => {
     e.preventDefault()
     let email = document.querySelector('#email').value
     let password = document.querySelector('#password').value
-    console.log(email, password)
+    
+    let json = {
+        email,
+        password
+    }
+
+    console.log(json)
+    
+    let stringJSON = JSON.stringify(json)
+    console.log(stringJSON)
+    
+    let jsonParse = JSON.parse(stringJSON)
+    console.log(jsonParse)
 })
