7d980cbad530b8f123200dd6d0418d4c5282f186
Wesley Anjos <wesleyanjoszero@gmail.com>
2025-04-28 10:26:13 -0300

    feat - percorrendo um array

--- Lista/js/script.js
+++ Lista/js/script.js
@@ -8,6 +8,18 @@ let data = [{
 }, {
     id: 3,
     title: 'Estudar JavaScript'
+}, {
+    id: 4,
+    title: 'Estudar PHP'
 }]
 
-console.log(data)
+data.forEach(task => {
+    let li = document.createElement('li')
+    li.innerHTML = 
+    `
+    <input type="checkbox" id="task-${task.id}">
+    <label for="task-${task.id}">${task.title}</label>
+    </input>
+    `
+    document.querySelector('.todo').append(li)
+})
