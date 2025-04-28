81df037791fb678fafd2e34360d8c27833760013
Wesley Anjos <wesleyanjoszero@gmail.com>
2025-04-28 10:41:05 -0300

    feat - adicionando itens na lista

--- Lista/js/script.js
+++ Lista/js/script.js
@@ -13,21 +13,42 @@ let data = [{
     title: 'Estudar PHP'
 }]
 
-data.forEach(task => {
-    let li = document.createElement('li')
-    li.innerHTML = 
-    `
-    <input type="checkbox" id="task-${task.id}">
-    <label for="task-${task.id}">${task.title}</label>
-    `
-    li.querySelector('input').addEventListener('change', e => {
-        
-        if (e.target.checked) {
-            li.classList.add('complete')
-        } else {
-            li.classList.remove('complete')
-        }
-        
+function renderTodo() {
+
+    document.querySelector('.todo').innerHTML = ''
+
+    data.forEach(task => {
+        let li = document.createElement('li')
+        li.innerHTML = 
+        `
+        <input type="checkbox" id="task-${task.id}">
+        <label for="task-${task.id}">${task.title}</label>
+        `
+        li.querySelector('input').addEventListener('change', e => {
+            
+            if (e.target.checked) {
+                li.classList.add('complete')
+            } else {
+                li.classList.remove('complete')
+            }
+    
+        })
+        document.querySelector('.todo').append(li)
     })
-    document.querySelector('.todo').append(li)
+
+}
+
+document.querySelector('#new-task').addEventListener('keyup', e => {
+    
+    if (e.key === 'Enter') {
+        data.push({
+            id: data.length + 1,
+            title: e.target.value
+        })
+        e.target.value = ''
+        renderTodo()
+    }
+
 })
+
+renderTodo()
